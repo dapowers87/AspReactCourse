b59f51a1c0ce8cc663ab13cabc4ec1cd6d45138a
David Powers <david.powers@newgistics.com>
2019-08-27 16:07:56 -0500

    Add MobX

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,122 @@
+import { observable, action, computed, configure, runInAction } from "mobx";
+import { createContext, SyntheticEvent } from "react";
+import { IActivity } from "../models/activity";
+import agent from "../api/agent";
+
+configure({ enforceActions: "always" });
+
+class ActivityStore {
+  @observable activityRegistry = new Map();
+  @observable activities: IActivity[] = [];
+  @observable selectedActivity: IActivity | undefined;
+  @observable loadingInitial = false;
+  @observable editMode = false;
+  @observable submitting = false;
+  @observable target = "";
+
+  @computed get activitiesByDate() {
+    return Array.from(this.activityRegistry.values()).sort(
+      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+  }
+
+  @action loadActivities = async () => {
+    this.loadingInitial = true;
+
+    try {
+      const activities = await agent.Activities.list();
+      runInAction("loading activities", () => {
+        activities.forEach(activity => {
+          activity.date = activity.date.split(".")[0];
+          this.activityRegistry.set(activity.id, activity);
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    runInAction("setting loadingIntial to false", () => {
+      this.loadingInitial = false;
+    });
+  };
+
+  @action createActivity = async (activity: IActivity) => {
+    this.submitting = true;
+    try {
+      await agent.Activities.create(activity);
+      runInAction("after create", () => {
+        this.activityRegistry.set(activity.id, activity);
+        this.editMode = false;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    runInAction("create: set submit to false", () => {
+      this.submitting = false;
+    });
+  };
+
+  @action openCreateForm = () => {
+    this.editMode = true;
+    this.selectedActivity = undefined;
+  };
+
+  @action openEditForm = (id: string) => {
+    this.selectedActivity = this.activityRegistry.get(id);
+    this.editMode = true;
+  };
+
+  @action cancelSelectedActivity = () => {
+    this.selectedActivity = undefined;
+  };
+
+  @action cancelFormOpen = () => {
+    this.editMode = false;
+  };
+
+  @action selectActivity = (id: string) => {
+    this.selectedActivity = this.activityRegistry.get(id);
+    this.editMode = false;
+  };
+
+  @action editActivity = async (activity: IActivity) => {
+    this.submitting = true;
+
+    try {
+      await agent.Activities.update(activity);
+      runInAction("after edit", () => {
+        this.activityRegistry.set(activity.id, activity);
+        this.selectedActivity = activity;
+        this.editMode = false;
+        this.submitting = false;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  @action deleteActivity = async (
+    event: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) => {
+    this.submitting = true;
+    this.target = event.currentTarget.name;
+
+    try {
+      await agent.Activities.delete(id);
+      runInAction("after delete", () => {
+        this.activityRegistry.delete(id);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    runInAction("delete: set target and submit", () => {
+      this.submitting = false;
+      this.target = "";
+    });
+  };
+}
+
+export default createContext(new ActivityStore());
--- client-app/src/features/Activities/form/ActivityForm.tsx
+++ client-app/src/features/Activities/form/ActivityForm.tsx
@@ -1,23 +1,20 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useContext } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
+import ActivityStore from "../../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 
 interface IProps {
-  setEditMode: (editMode: boolean) => void;
   activity: IActivity;
-  createActivity: (activity: IActivity) => void;
-  editActivity: (activity: IActivity) => void;
-  submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
-  setEditMode,
   activity: initialFormState,
-  createActivity,
-  editActivity,
-  submitting
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const {createActivity, submitting, editActivity, cancelFormOpen} = activityStore;
+  
   const initializeForm = () => {
     if (initialFormState) {
       return initialFormState;
@@ -109,11 +106,11 @@ const ActivityForm: React.FC<IProps> = ({
           floated="right"
           type="button"
           content="Cancel"
-          onClick={() => setEditMode(false)}
+          onClick={cancelFormOpen}
         />
       </Form>
     </Segment>
   );
 };
 
-export default ActivityForm;
+export default observer(ActivityForm);
