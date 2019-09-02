60c2cd512c692987a70a9c35ec28f3ab150b0cf6
David Powers <david.powers@newgistics.com>
2019-09-02 17:50:34 -0500

    End of Section 11: Forms

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM mcr.microsoft.com/dotnet/core/aspnet:2.2 AS base
 WORKDIR /app
 #ENV ASPNETCORE_ENVIRONMENT=Production
-ENV ASPNETCORE_URLS http://*:5000
+ENV ASPNETCORE_URLS http://*:5050
 EXPOSE 5000
 
 FROM mcr.microsoft.com/dotnet/core/sdk:2.2 AS builder
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,8 @@ import { observable, action, computed, configure, runInAction } from "mobx";
 import { createContext, SyntheticEvent } from "react";
 import { IActivity } from "../models/activity";
 import agent from "../api/agent";
+import { history } from "../..";
+import { toast } from "react-toastify";
 
 configure({ enforceActions: "always" });
 
@@ -20,14 +22,21 @@ class ActivityStore {
 
   groupActivitiesByDate(activities: IActivity[]) {
     const sortedActivities = activities.sort(
-      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+      (a, b) => a.date.getTime() - b.date.getTime()
     );
 
-    return Object.entries(sortedActivities.reduce((activities, activity) => {
-      const date = activity.date.split('T')[0];
-      activities[date] = activities[date] ? [...activities[date], activity] : [activity];
-      return activities;
-    }, {} as {[key: string]: IActivity[]}))
+    return Object.entries(
+      sortedActivities.reduce(
+        (activities, activity) => {
+          const date = activity.date.toISOString().split("T")[0];
+          activities[date] = activities[date]
+            ? [...activities[date], activity]
+            : [activity];
+          return activities;
+        },
+        {} as { [key: string]: IActivity[] }
+      )
+    );
   }
 
   @action loadActivities = async () => {
@@ -37,7 +46,7 @@ class ActivityStore {
       const activities = await agent.Activities.list();
       runInAction("loading activities", () => {
         activities.forEach(activity => {
-          activity.date = activity.date.split(".")[0];
+          activity.date = new Date(activity.date);
           this.activityRegistry.set(activity.id, activity);
         });
       });
@@ -55,16 +64,22 @@ class ActivityStore {
 
     if (activity) {
       this.activity = activity;
+      return activity;
     } else {
       this.loadingInitial = true;
       try {
         activity = await agent.Activities.details(id);
 
         runInAction("loadActivity: getting activity", () => {
+          activity.date = new Date(activity.date);
           this.activity = activity;
+          this.activityRegistry.set(activity.id, activity);
+          this.loadingInitial = false;
         });
+
+        return activity;
       } catch (error) {
-        throw error;
+        console.log(error);
       }
 
       runInAction("set loading to false", () => {
@@ -88,8 +103,10 @@ class ActivityStore {
       runInAction("after create", () => {
         this.activityRegistry.set(activity.id, activity);
       });
+      history.push(`/activities/${activity.id}`);
     } catch (error) {
-      console.log(error);
+      toast.error("Problem submitting data");
+      console.log(error.response);
     }
 
     runInAction("create: set submit to false", () => {
@@ -104,9 +121,10 @@ class ActivityStore {
       runInAction("after edit", () => {
         this.activityRegistry.set(activity.id, activity);
         this.activity = activity;
+        history.push(`/activities/${activity.id}`);
       });
     } catch (error) {
-      console.log(error);
+      console.log(error.response);
     }
 
     runInAction("editActivity: false submitting", () => {
